35077714c676017e2791577847fb0365d1024405
AdamiMath <matheusadami.lima@gmail.com>
2024-08-29 15:29:40 -0300

    criacao header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login / Criar</Link>
+
+    </div>
   )
 }
 
