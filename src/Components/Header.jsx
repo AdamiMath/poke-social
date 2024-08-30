9cfb6a7cf18f3887e5c45b6bb87f79f703bf189f
AdamiMath <matheusadami.lima@gmail.com>
2024-08-30 11:56:06 -0300

    css geral

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div className={styles.header}>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login / Criar</Link>
-
+      <nav className='container'>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Criar</Link>
+      </nav>
     </div>
   )
 }
