5df45b384afef04105d85b1871bb3b05ff309a89
AdamiMath <matheusadami.lima@gmail.com>
2024-09-05 12:59:27 -0300

    atualização de rotas

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -11,7 +11,7 @@ const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const {userLogin} = useContext(UserContext);
+  const {userLogin, error, loading} = useContext(UserContext);
 
  async function handleSubmit(e){
     e.preventDefault();
@@ -23,12 +23,14 @@ const LoginForm = () => {
 
 
   return (
-    <section>
+    <section className="animeLeft">
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username}/>
         <Input label="Senha" type="password" name="password" {...password}/>       
-        <Buttons>Entrar</Buttons>
+        {loading ?<Buttons disabled>Carregando...</Buttons> : <Buttons>Entrar</Buttons> }
+        
+        {error && <p>{error}</p> }
 
       </form>
      <Link to="/login/criar">Criar Login</Link>
