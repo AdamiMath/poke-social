0aec051273918f13edd6821543fcdb36b6adc0d7
AdamiMath <matheusadami.lima@gmail.com>
2024-09-06 13:24:44 -0300

    criação do menu mobile

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -6,19 +6,29 @@ import Adicionar from '../../assets/adicionar.svg?react';
 import Estatisticas from '../../assets/estatisticas.svg?react';
 import Feed from '../../assets/feed.svg?react';
 import Sair from '../../assets/sair.svg?react';
+import useMedia from '../../Hooks/useMedia';
 
 
 
 const UserHeaderNav = () => {
-    const[mobile, setMobile] = useState(null);
-    const {userLogout} = useContext(UserContext)
+ 
+    const {userLogout} = useContext(UserContext);
+    const mobile = useMedia('(max-width: 40rem)');
+    const [mobileMenu, setMobileMenu] = useState(false)
+
+
+
     const navigate = useNavigate();
     
     function handleLogout(){
       userLogout();
       navigate('/login');
     }
+
+
   return (
+    <>
+    {mobileMenu && <button className={style.mobileButton} aria-label='Menu' onClick={() => setMobileMenu(!mobileMenu)}></button>}
     <nav className={style.nav}>
         <NavLink to="/conta" end><Feed />{mobile && 'Minhas Fotos'}</NavLink>
         <NavLink to="/conta/estatistica"><Estatisticas /> {mobile && 'Estatísticas' }</NavLink>
@@ -27,6 +37,7 @@ const UserHeaderNav = () => {
 
 
     </nav>
+    </>
   )
 }
 
