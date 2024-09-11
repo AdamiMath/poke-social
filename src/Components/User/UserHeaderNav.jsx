a826f6480f3a80159aeb7aace15da66176f42f87
AdamiMath <matheusadami.lima@gmail.com>
2024-09-10 21:31:23 -0300

    criacao dos mobile

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,44 +1,59 @@
-import React, { useContext, useState } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom'
-import { UserContext } from '../../UserContext'
-import style from './UserHeader.module.css'
+import React, { useContext, useEffect, useState } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import style from './UserHeader.module.css';
 import Adicionar from '../../assets/adicionar.svg?react';
 import Estatisticas from '../../assets/estatisticas.svg?react';
 import Feed from '../../assets/feed.svg?react';
 import Sair from '../../assets/sair.svg?react';
 import useMedia from '../../Hooks/useMedia';
 
-
-
 const UserHeaderNav = () => {
- 
-    const {userLogout} = useContext(UserContext);
-    const mobile = useMedia('(max-width: 40rem)');
-    const [mobileMenu, setMobileMenu] = useState(false)
+  const { userLogout } = useContext(UserContext);
+  const mobile = useMedia('(max-width: 40rem)');
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-    
-    function handleLogout(){
-      userLogout();
-      navigate('/login');
-    }
-
+  function handleLogout() {
+    userLogout();
+    navigate('/login');
+  }
 
   return (
     <>
-    {mobileMenu && <button className={style.mobileButton} aria-label='Menu' onClick={() => setMobileMenu(!mobileMenu)}></button>}
-    <nav className={style.nav}>
-        <NavLink to="/conta" end><Feed />{mobile && 'Minhas Fotos'}</NavLink>
-        <NavLink to="/conta/estatistica"><Estatisticas /> {mobile && 'Estatísticas' }</NavLink>
-        <NavLink to="/conta/postar"><Adicionar />{mobile && 'adicionar'}</NavLink>
-        <button onClick={handleLogout}><Sair />{mobile && 'sair'}</button>
-
-
-    </nav>
+      {mobile && (
+        <button
+          className={`${style.mobileButton} ${mobileMenu ? style.mobileButtonActive : ''}`}
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        />
+      )}
+      <nav className={`${mobile ? style.navMobile : style.nav} ${mobileMenu && mobile ? style.navMobileActive : ''}`}>
+        <NavLink to="/conta" end>
+          <Feed />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatistica">
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <Adicionar />
+          {mobile && 'Adicionar'}
+        </NavLink>
+        <button onClick={handleLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default UserHeaderNav
+export default UserHeaderNav;
--- src/Components/User/UserPostPhoto.jsx
+++ src/Components/User/UserPostPhoto.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import style from './UserPost.module.css'
+import useForm from '../../Hooks/useForm'
+
+import Input from '../Forms/Input'
+import Buttons from '../Forms/Buttons'
+
 
 const UserPostPhoto = () => {
+
+  const nome = useForm();
+  const peso = useForm();
+  const idade = useForm();
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  function handleImgChange(){
+    
+  }
+
   return (
-    <div>UserPostPhoto</div>
+    <section className={`${style.photoPost} animeLeft`}>
+      <form onSubmit={handleSubmit}>
+        <Input label="photo" type="text" name="nome"/>
+        <Input label="peso" type="text" name="peso"/>
+        <Input label="Idade" type="text" name="idade"/>
+        <input type="file" name="img" id="img" onChange={handleImgChange}/>
+        <Buttons>Enviar</Buttons>
+
+      </form>
+    </section>
   )
 }
 
